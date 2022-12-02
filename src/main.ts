84184954e6445451be91695ce1807e053e589387
Johan Karlsson <johan.k0528@gmail.com>
2022-12-02 14:03:43 +0100

    Add scroll to top button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import './style.css';
+
+// Get distance to top of site
+const getDistanceToTop = () => {
+  return document.documentElement.scrollTop;
+};
+
+// Scroll listener
+document.addEventListener('scroll', () => {
+  const distance = getDistanceToTop();
+  if (distance > 200) {
+    document.querySelector('.direction_indicator')!.classList.add('visible');
+  } else {
+    document.querySelector('.direction_indicator')!.classList.remove('visible');
+  }
+});
